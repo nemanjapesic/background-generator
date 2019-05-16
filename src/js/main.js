1b116c665914616d33e51729fdedb5cf39030d5f
Nemo <nemanja992@gmail.com>
2019-05-16 11:45:06 +0200

    fixed formatting

--- src/js/main.js
+++ src/js/main.js
@@ -5,53 +5,55 @@ const COLOR1 = document.querySelector("#color1");
 const COLOR2 = document.querySelector("#color2");
 const SLIDER = document.querySelector("#slider");
 const CSS_TEXT = document.querySelector("#css-text");
-const BTN_RANDOM = document.querySelector("#btn-random")
+const BTN_RANDOM = document.querySelector("#btn-random");
 
 // Set Background Gradient and display CSS code
 const setGradientBackground = () => {
-    let linearGradient = `linear-gradient(${SLIDER.value}deg, ${COLOR1.value}, ${COLOR2.value})`;
-    BODY.style.background = linearGradient;
-    CSS_TEXT.textContent = `${linearGradient};`;
-}
+  let linearGradient = `linear-gradient(${SLIDER.value}deg, ${COLOR1.value}, ${
+    COLOR2.value
+  })`;
+  BODY.style.background = linearGradient;
+  CSS_TEXT.textContent = `${linearGradient};`;
+};
 
 // Generate Random HEX color value
 const getRandomColor = () => {
-    let hexValues = "0123456789ABCDEF";
-    let color = "#";
-    for(let i = 0; i < 6; i++){
-        color += hexValues[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+  let hexValues = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hexValues[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
 // Set Color Inputs to Random Colors and setGradientBackground
 const generateRandomGradient = () => {
-    COLOR1.value = getRandomColor();
-    COLOR2.value = getRandomColor();
-    SLIDER.value = Math.floor(Math.random() * 361);
-    setGradientBackground();
-}
+  COLOR1.value = getRandomColor();
+  COLOR2.value = getRandomColor();
+  SLIDER.value = Math.floor(Math.random() * 361);
+  setGradientBackground();
+};
 
 // Copy Text to Clipboard
 const copyText = () => {
-    // Select the text field
-    CSS_TEXT.select();
-    // Copy the Text
-    document.execCommand("copy");
+  // Select the text field
+  CSS_TEXT.select();
+  // Copy the Text
+  document.execCommand("copy");
 
-    showToast();
-}
+  showToast();
+};
 
 // Show Toast
 const showToast = () => {
-    // Set Toast Text and Show Toast
-    TOAST.textContent = "Copied to clipboard";
-    TOAST.classList.add("show");
-    // Hide toast after 3 seconds
-    setTimeout(() => {
-        TOAST.classList.remove("show");
-    }, 3000);
-}
+  // Set Toast Text and Show Toast
+  TOAST.textContent = "Copied to clipboard";
+  TOAST.classList.add("show");
+  // Hide toast after 3 seconds
+  setTimeout(() => {
+    TOAST.classList.remove("show");
+  }, 3000);
+};
 
 // Set Initial Background to Random Gradient
 generateRandomGradient();
